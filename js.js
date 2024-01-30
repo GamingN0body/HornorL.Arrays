9f08d456fc9edf0773c0ef46eb97ae55481f4911
HLoghan <LegoSaber06@gmail.com>
2024-01-30 14:03:10 -0800

    done

---
+++ js.js
@@ -0,0 +1,39 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn Kubernetes'];
+newYearsResolutions[0] = 'Learn from my mistakes' //changes the index
+newYearsResolutions[1] = 'Get in good shape'
+newYearsResolutions[2] = 'Get good grades'
+console.log(newYearsResolutions);
+/*
+Arrrays: stores information
+        used to make lists, JS
+            can store data types, the list will be ordered
+        can store any data
+        index
+            number in array
+            first item = 0
+        elements
+            strings
+            number
+            boolian
+                to call an element
+                    hoobies[0]
+    Array Literal: 
+        directly define the items
+        ['element', element, element]
+            if same element no need ,
+
+
+*/
+
+const hobbies = [`martial art`, `football`, `fencing`] // use const to define not let or var
+console.log(hobbies);
+listItem = hobbies[0];
+listItem = hobbies[1];
+listItem = hobbies[2];
+console.log(hobbies[0]);
+console.log(hobbies[1]);
+console.log(hobbies[2]);
+
+let seasons= ['Winter', 'Spring', 'Summer', 'Fall'];
+seasons[3] = 'Autumn'
+console.log(seasons);
